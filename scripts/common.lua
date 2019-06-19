2f8e18f99f5d573ca24748469f6aa3933750a5d3
Zach Day <zachday@Zachs-iMac.local>
2019-06-19 15:25:40 -0400

    Basic workable state
    No animations or anything, just titlescreen

--- scripts/common.lua
+++ scripts/common.lua
@@ -1,1 +1,9 @@
 gfx.LoadSkinFont("NotoSans-Regular.ttf");
+
+-- Quick log.
+Qlog = {
+    i = function(text) game.Log(text, game.LOGGER_INFO) end,
+    n = function(text) game.Log(text, game.LOGGER_NORMAL) end,
+    w = function(text) game.Log(text, game.LOGGER_WARNING) end,
+    e = function(text) game.Log(text, game.LOGGER_ERROR) end
+}
--- scripts/titlescreen.lua
+++ scripts/titlescreen.lua
@@ -1,71 +1,44 @@
-WIN_X, WIN_Y = game.GetResolution()
+-- We use 1080p as our base measurement.
+WIN_WIDTH, WIN_HEIGHT = game.GetResolution()
 
-MAIN_BUTTON_LINE_DIFF = 10
-MAIN_BUTTON_VOFFSET = 100
-MAIN_BUTTON_VSPACING = 20
+-- Logo is shown at top of screen.
+local LOGO_TOP_OFFSET = 200
 
-COLOR_MAIN_BUTTON_INACTIVE = {180, 180, 180}
-COLOR_MAIN_BUTTON_HOVER = {255, 255, 255}
+--- Main buttons are arranged vertically below the title logo.
+-- Offset of first main button from logo.
+local MAIN_BUTTON_VOFFSET = 100
+-- Spacing between bottom of a main button and top of the next.
+local MAIN_BUTTON_VSPACING = 20
+local MAIN_BUTTON_FONT_SZ = 40
 
-ANIM_MAIN_BUTTON_LINE_RESIZE_TIME = 0.3
+local COLOR_MAIN_BUTTON_INACTIVE = {180, 180, 180}
+local COLOR_MAIN_BUTTON_HOVER = {255, 255, 255}
 
-local time = 0
-
-main_button_new = function(label_text)
+main_button_new = function(btn_text, btn_action)
     return {
-        text = label_text,
-        action = nil,
-        label_inactive = nil,
-        label_hover = nil,
-        hover_start_moment = nil,
-        hover_end_moment = nil,
+        text = btn_text,
+        action = btn_action,
+        label = gfx.CreateLabel(btn_text, MAIN_BUTTON_FONT_SZ, 0)
     }
 end
 
-local main_buttons = {
-    [1] = main_button_new("Start"),
-    [2] = { text = "Settings" },
-    [3] = { text = "Exit" }
-}
-local hovered_idx = nil
+local main_buttons = {}
+local logo_label = nil
 
 -- Calculate the rectangle of a main button given its index.
 -- This assumes all main buttons are the same height.
-function button_bounds(idx)
-    width, height = gfx.LabelSize(main_buttons[idx].label_inactive)
-    x = WIN_X / 2 - width / 2
+button_bounds = function(idx)
+    width, height = gfx.LabelSize(main_buttons[idx].label)
+    x = WIN_WIDTH / 2 - width / 2
     y = MAIN_BUTTON_VOFFSET + (height + MAIN_BUTTON_VSPACING) * idx
     return x, y, width, height
 end
 
-function anim_proportion(start, duration)
-    if not start then return 1 end
-    if time < duration then return 1 end
-    return math.min(1, (time - start) / duration)
-end
-
-local init_done = false
 -- Performs setup logic that can't be done at initial interpretation.
 init = function()
-    -- `Menu` isn't accessible at the start so we have to wait until the first `render`.
-    main_buttons[1].action = Menu.Start
-    main_buttons[2].action = Menu.Settings
-    main_buttons[3].action = Menu.Exit
-
-    for i, btn in ipairs(main_buttons) do
-        game.Log("Creating label with text " .. btn.text, game.LOGGER_INFO)
-
-        gfx.FillColor(table.unpack(COLOR_MAIN_BUTTON_INACTIVE))
-        btn.label_inactive = gfx.CreateLabel(btn.text, 40, 0)
-
-        gfx.FillColor(table.unpack(COLOR_MAIN_BUTTON_HOVER))
-        btn.label_hover = gfx.CreateLabel(btn.text, 40, 0)
-
-        x, y, width, height = button_bounds(i)
-        game.Log("Button bounds: " .. x .. ", " .. y .. ", " .. width .. ", " .. height, game.LOGGER_INFO)
-    end
-
-    init_done = true
+    main_buttons[1] = main_button_new("Start", Menu.Start)
+    main_buttons[2] = main_button_new("Settings", Menu.Settings)
+    main_buttons[3] = main_button_new("Exit", Menu.Exit)
 end
 
 -- Check whether the mouse is within the rectangle.
@@ -74,59 +47,52 @@ mouse_intersects = function(x, y, w, h)
     return mposx > x and mposy > y and mposx < x+w and mposy < y+h;
 end
 
+draw_logo = function()
+    local LOGO_FONT_SZ = 70
+    if not logo_label then
+        logo_label = gfx.CreateLabel("UUSCS", LOGO_FONT_SZ, 0)
+    end
+
+    width, height = gfx.LabelSize(logo_label)
+    gfx.FillColor(255, 255, 255)
+    gfx.DrawLabel(logo_label, WIN_WIDTH / 2 - width / 2, 0)
+end
+
 -- Draw a button centered horizontally.
-draw_button = function(idx)
-    btn = main_buttons[idx]
+draw_button = function(idx, btn)
     x, y, width, height = button_bounds(idx)
 
     if mouse_intersects(x, y, width, height) then
-        btn.hover_end_moment = nil
-        if btn.hover_start_moment == nil then
-            btn.hover_start_moment = time
-        end
-
+        -- Hover state.
         gfx.FillColor(table.unpack(COLOR_MAIN_BUTTON_HOVER))
-        gfx.DrawLabel(main_buttons[idx].label_hover, x, y)
-        
-        local anim = anim_proportion(btn.hover_start_moment, ANIM_MAIN_BUTTON_LINE_RESIZE_TIME)
+        gfx.DrawLabel(main_buttons[idx].label, x, y)
 
-        -- Draw box border.
         gfx.BeginPath()
-        gfx.MoveTo(x + width / 2)
-        gfx.LineTo(x + width / 2 + (width / 2) * min(1, )
+        gfx.MoveTo(x + 10, y + height)
+        gfx.LineTo(x + width - 10, y + height)
         gfx.StrokeColor(table.unpack(COLOR_MAIN_BUTTON_HOVER))
         gfx.Stroke()
     else
-        btn.hover_start_moment = nil
-        if btn.hover_end_moment == nil then
-            btn.hover_end_moment = time
-        end
-
-        local anim = anim_proportion(btn.hover_end_moment, ANIM_MAIN_BUTTON_LINE_RESIZE_TIME)
-
+        -- Inactive state.
         gfx.FillColor(table.unpack(COLOR_MAIN_BUTTON_INACTIVE))
-        gfx.DrawLabel(main_buttons[idx].label_inactive, x, y)
-
-        gfx.BeginPath()
-        gfx.MoveTo(x + MAIN_BUTTON_LINE_DIFF * anim, y + height)
-        gfx.LineTo(x + width - MAIN_BUTTON_LINE_DIFF * anim, y + height)
-        gfx.StrokeColor(table.unpack(COLOR_MAIN_BUTTON_INACTIVE))
-        gfx.Stroke()
+        gfx.DrawLabel(main_buttons[idx].label, x, y)
     end
 end
 
+local needs_init = true
 -- Render the screen.
 render = function(deltaTime)
-    if not init_done then init() end
-
-    hovered_idx = nil
-
-    for i, btn in ipairs(main_buttons) do
-        -- Draw buttons.
-        draw_button(i)
+    if needs_init then 
+        init() 
+        needs_init = false
     end
 
-    time = time + deltaTime
+    draw_logo()
+
+    -- Draw buttons.
+    for i, btn in ipairs(main_buttons) do
+        draw_button(i, btn)
+    end
 end
 
 -- Callback on mouse presses.
@@ -135,7 +101,8 @@ mouse_pressed = function(button)
         local x, y, w, h = button_bounds(i)
         if mouse_intersects(x, y, w, h) then
             main_buttons[i].action()
-            game.Log("Button " .. i .. " clicked.", game.LOGGER_INFO)
+            Qlog.i("Button " .. i .. " clicked.")
+            return 0
         end
     end
     return 0
